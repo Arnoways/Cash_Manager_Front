c007e8c98483887468700ed13f638f0685108f08
Arthur <arthut.h72@gmail.com>
2019-10-15 10:26:52 +0200

    Écran android
    Premier écran

---
+++ app/src/main/java/com/example/cashmanagerfront/ChoosePayementType.kt
@@ -0,0 +1,12 @@
+package com.example.cashmanagerfront
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChoosePayementType : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_payement_type)
+    }
+}
