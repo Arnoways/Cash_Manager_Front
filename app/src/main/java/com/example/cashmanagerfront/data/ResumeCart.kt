305e1f19cef4fad632823d359bf08efddbd99c05
mdufour <mathieu.dufour@whereyoulove.com>
2019-10-15 16:01:26 +0200

    import R in view so the project can build

--- app/src/main/java/com/example/cashmanagerfront/data/ResumeCart.kt
+++ app/src/main/java/com/example/cashmanagerfront/data/ResumeCart.kt
@@ -2,6 +2,7 @@ package com.example.cashmanagerfront.data
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cashmanagerfront.R
 
 class ResumeCart : AppCompatActivity() {
 
