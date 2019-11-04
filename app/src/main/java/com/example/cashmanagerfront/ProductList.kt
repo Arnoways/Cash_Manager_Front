65d8e7cdcc6015c157bdd474f16764b0d41383af
mdufour <mathieu.dufour@whereyoulove.com>
2019-11-04 17:22:21 +0100

    display product list in view done - need to do some styling on it

--- app/src/main/java/com/example/cashmanagerfront/ProductList.kt
+++ app/src/main/java/com/example/cashmanagerfront/ProductList.kt
@@ -1,5 +1,7 @@
 package com.example.cashmanagerfront
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TableLayout
@@ -9,21 +11,23 @@ import com.example.cashmanagerfront.data.ProductDataSource
 import com.example.cashmanagerfront.data.model.Product
 
 import android.util.Log
+import android.util.TypedValue
+import androidx.core.view.setPadding
 
 class ProductList : AppCompatActivity() {
 
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
 
-        var productList: MutableList<Product> = ProductDataSource().loadProducts()
+        var productList: MutableList<Product> = ProductDataSource(this).loadProducts()
         System.out.println("Product list : " + productList.toString())
         // get Table Layout
         var tableLayout: TableLayout = findViewById(R.id.tableLayout)
 
         var index = 0
         while(index < productList.size) {
-            System.out.println("Index de la boucle :" + index)
             // create a row
             var row: TableRow = TableRow(this)
             // parameters for the row
@@ -33,13 +37,16 @@ class ProductList : AppCompatActivity() {
             val tvname = TextView(this)
             val tvprice = TextView(this)
             // set the text values
-            tvname.setText(productList[index].name.toString())
+            tvname.setText(productList[index].name)
             tvprice.setText(productList[index].price.toString())
+            // stylish text view
+            tvname.setTextAppearance(R.style.textViewStyle)
+            tvprice.setTextAppearance(R.style.textViewStyle)
             // add it to our row
             row.addView(tvname, 0)
             row.addView(tvprice,1)
             // add row to our table
-            tableLayout.addView(row, index)
+            tableLayout.addView(row, index+1)
             index++
         }
 
--- app/src/main/java/com/example/cashmanagerfront/data/ProductDataSource.kt
+++ app/src/main/java/com/example/cashmanagerfront/data/ProductDataSource.kt
@@ -1,18 +1,30 @@
 package com.example.cashmanagerfront.data
 
+import android.app.Activity
 import com.example.cashmanagerfront.data.model.Product
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
 
 
-class ProductDataSource() {
-    var products: MutableList<Product> = ArrayList();
+class ProductDataSource(var myActivity: Activity) {
+    var products: MutableList<Product> = ArrayList()
+
 
     fun loadProducts(): MutableList<Product> {
-        // get the json file
-        File("@res/products.txt").forEachLine {
-            val datas: List<String> = it.split(";")
 
+        // get the json file
+        val inputStream:InputStream = myActivity.assets.open("products.txt")
+        val inputStreamReader = InputStreamReader(inputStream)
+        var line: String?
+        val br = BufferedReader(inputStreamReader)
+        line = br.readLine()
+        while (line != null) {
+            val datas: List<String> = line.split(";")
+            System.out.println(datas.toString())
             products.add(Product(datas[0].toInt(), datas[1], datas[2].toDouble()))
+            line = br.readLine()
         }
 
         return products
