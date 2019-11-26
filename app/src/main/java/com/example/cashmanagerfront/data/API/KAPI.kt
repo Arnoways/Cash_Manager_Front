1b0a518ce6ffe0d034f996eceeaee8b8deeea42d
Arthur <arthut.h72@gmail.com>
2019-11-26 11:31:16 +0100

    Formalisation API class
    Ajout de l'adresse du serveur et correction pour correspondre à la documentation de l'api

--- app/src/main/java/com/example/cashmanagerfront/data/API/KAPI.kt
+++ app/src/main/java/com/example/cashmanagerfront/data/API/KAPI.kt
@@ -2,42 +2,91 @@ package com.example.cashmanagerfront.data.API
 
 import khttp.*
 import org.json.JSONObject
-import com.example.cashmanagerfront.data.model.Product
 import com.example.cashmanagerfront.data.model.Cart
+import org.json.JSONArray
 
 
+val serveurRoute = "http://3.231.177.119:8080"
 
-val defaultRoute = ""
+/***
+ *
+ * Gestion de l'authentification
+ *
+ */
+private fun signIn(email : String, pwd : String): Int {
 
+    val url = serveurRoute + "/api/signIn"
+    val payload = mapOf("email" to email, "pwd" to pwd)
+    val r = post(url, data= JSONObject(payload))
 
-private fun login(username : String, password : String) {
+    return  r.jsonObject.getInt("userID")
+}
 
-    val url = "/login"
-    val payload = mapOf("username" to username, "password" to password)
+private fun signOut(userId : Int) {
+
+    val url = serveurRoute + "/api/signOut/"+userId
+    val payload = mapOf("userId" to userId)
     val r = post(url, data= JSONObject(payload))
 }
 
-private fun getAllProduct() {
+private fun signUp(email : String, login: String, pwd : String): Int {
 
-    var url = defaultRoute + "/api/product"
-    var r = get(url)
+    val url = serveurRoute + "/api/signIn"
+    val payload = mapOf("email" to email, "pwd" to pwd, "login" to login)
+    val r = post(url, data= JSONObject(payload))
 
-    val productList : JSONObject = r.jsonObject
+    return  r.jsonObject.getInt("userID")
 
 }
 
-private fun getAllProduct(idProduct: Int) {
+/***
+ *
+ * Gestion des produits
+ *
+ */
 
-    var url = defaultRoute +"api/product/"+idProduct
+private fun getAllProduct(): JSONArray {
+
+    var url = serveurRoute + "/api/product"
+    var r = get(url)
+
+    val productList : JSONArray = r.jsonArray
+
+    return productList
+}
+
+private fun getAllProduct(idProduct: Int): JSONObject {
+
+    var url = serveurRoute +"api/product/"+idProduct
     var r = get(url)
 
     val product : JSONObject = r.jsonObject
+
+    return product
 }
+
+
+
+/***
+ *
+ * Gestion de panier
+ *
+ */
 
 private fun postCart(cart: Cart) {
 
-    val url = defaultRoute + "/api/cart"
+    val url = serveurRoute + "/api/cart"
     val payload = mapOf("cart" to cart)
 
-    val cart = post(url, data=JSONObject(payload))
+    val r = post(url, data=JSONObject(payload))
 }
+
+/**
+private fun putCart(cartId: Int) {
+
+    val url = serveurRoute + "/api/carts/"+cartId
+    val payload = mapOf("cart" to cart)
+
+    val r = post(url, data=JSONObject(payload))
+}
+*/
